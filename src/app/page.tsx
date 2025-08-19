e6b204d348c8f8514c5e994128b354f2d227bb82
Fayiza <fayiza.kt@gmail.com>
2025-08-19 17:36:44 +0400

    main page file modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,64 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React from 'react'
+import CarousalSection from './components/CarousalSection/CarousalSection';
+import './styles/CommonLayout.css'
+import { Metadata } from 'next';
+import Image from 'next/image';
+
+
+export const metadata: Metadata = {
+  title: "Quickart - Home",
+  description: "Welcome to Quickart - Your Stylish Destination for Smart Shopping!",
+};
+
+const offers = [
+  { title: "50% Off on School Bags", description: "Grab the deal before it ends!" },
+  { title: "Fashion Bonanza", description: "Flat 40% Off on Clothing" }
+];
+
+const CaptionStyle:any = {
+  color:"blue",
+  fontStyle:"italic",
+  textAlign:"center",
+  textDecoration:"underline"
+}
+
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <CarousalSection />
+      <h2 style={CaptionStyle}>Kart It Quick, Love It Long.</h2>
+      <h4 className="text-3xl font-bold text-center">Welcome to Quickart – Your Stylish Destination for Smart Shopping!</h4>
+      <p>Discover a world of fashion, electronics, beauty, and lifestyle products curated just for you. At QuicKart, we combine luxury, style, and affordability in one seamless shopping experience.Browse our collection of quality products. Free delivery on first order</p>
+      
+      <div className="container mt-4">
+      <h3> Today's Offers </h3>
+      <div className="row">
+        {offers.map((offer, index) => (
+          <div key={index} className="col-md-6">
+            <div className="card mb-3">
+              <div className="card-body">
+                <h5 className="card-title">{offer.title}</h5>
+                <p className="card-text">{offer.description}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="row">
+      <div className="col-md-4">
+          <Image src="/images/b2s.jpg" alt="Offer Image3" className="img-fluid" width={500} height={100} />
+      </div>
+      <div className="col-md-4">
+          <Image src="/images/fashion.jpeg" alt="Offer Image3" className="img-fluid" width={500} height={100} />
+      </div>
+      <div className="col-md-4">
+          <Image src="/images/b2s.jpg" alt="Offer Image4" className="img-fluid" width={500} height={100} />
+      </div>
+      </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    </div>
+
     </div>
   );
 }
